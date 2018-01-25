415a98bfedfb31ab0d3d79bc6efc7767c914cdd7
JJD <vanDrunen.julia@gmail.com>
2018-01-25 16:38:35 +0100

    update 3
    altered stylesheet, cleaned up code a bit

--- script.js
+++ script.js
@@ -1,69 +1,42 @@
 
-/* gather your data from the form via DOM
-organise it in an object or array
-generate JSON with JSON.stringify
-POST it with XMLHttpRequest */
+//GLOBAL VARIABLES
 var xhr = new XMLHttpRequest();
-
-var uname = "";
+var uname = ""; // username, overwritten bij start()
 
 function start(){
 uname = document.getElementById("usernameinput").value;
-
 document.getElementById("username").style.display = "none";
 document.getElementById("allchat").style.display = "block";
 window.setInterval(function(){ // calls refreshchat every 3 seconds, so chat is updated.
 	getmessages();
 }, 1000);   
-
-
 }
 
-
-
-function getmessages(){
-	//console.log("getting messages");
- 	xhr.open('GET', 'api.php', true);
+function getmessages(){ //gets messages from api.php (.txt file)
+	xhr.open('GET', 'api.php', true);
  	xhr.onreadystatechange = function () {
   if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-    //console.log(xhr.responseText);
-    var allmessages=xhr.response;
-	document.getElementById("messages").innerHTML = allmessages.replace(/(?:\r\n|\r|\n)/g, '<br />');
-	scrollToBottom(); 
+    var allmessages=xhr.response; //messages arrive as one giant string, separated by "ENTER"
+	document.getElementById("messages").innerHTML = allmessages.replace(/(?:\r\n|\r|\n)/g, '<br />'); //writes messages to div, replaces enter with <br/>
+	scrollToBottom(); //calls scroll function so newest messages are shown
   	}
 	};
  	xhr.send();
- 	
 };
 
-
-function sendmessages(){
-	console.log("sending messages")
-	
-	var msg = "(" +time +") " +chatform.message.value; 
+function sendmessages(){ //sends new messages to api.php, so they can be added to the .txt file.
 	var d = new Date();
 	var time = d.toLocaleTimeString();
-	var msg =  chatform.message.value; 
-	var unameTime="(" +time +") "+uname;
-	var url= "username="+ unameTime +"&message=" + msg;
-	console.log(url);
-	xhr.open("POST","api.php", true);
-	
-  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.send(url);
-
-	
-	document.getElementById('inputMessage').value = "";
+	var msg =  chatform.message.value;  //gets message from form
+	var unameTime="(" +time +") "+uname; //combines username with timestamp
+	var url= "username="+ unameTime +"&message=" + msg; //generates POST url with username and message.
+	xhr.open("POST","api.php", true); //POST request
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 
+  	xhr.send(url);
+	document.getElementById('inputMessage').value = ""; //resets message input field.
 }; 
 
-
- 
-
-
-
- 
-
- function scrollToBottom(){ //makes sure the chat div automatically scrolls to the bottom (thanks Ewout!)
+function scrollToBottom(){ //makes sure the chat div automatically scrolls to the bottom 
    var div = document.getElementById("messages");
    div.scrollTop = div.scrollHeight - div.clientHeight;
 } 
