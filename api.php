041803121370de0742891a544e299cd473eda222
JJD <vanDrunen.julia@gmail.com>
2018-01-26 17:22:13 +0100

    try hard

--- api.php
+++ api.php
@@ -1,19 +1,121 @@
 <?php
 	
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $uname = $_POST["username"]; 
-    $msg = $_POST["message"];
-    $file = fopen('chat.txt', 'a+'); //Open your .txt file
-    $content = $uname. " : " .$msg. PHP_EOL;  // merges username and message
-    fwrite($file , $content); // adds message to .txt
-    fclose($file ); //closes .txt
-if(isset($_SERVER['HTTP_REFERER'])) {
-    $previous = $_SERVER['HTTP_REFERER'];
-}
-   die(header("Location: ".$_SERVER["HTTP_REFERER"]));
-} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') { //does stuff if the request method is get
-    $myfile = fopen("chat.txt", "r") or die("Unable to open file!");
-    echo fread($myfile,filesize("chat.txt"));
-    fclose($myfile);
-}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $dsn = 'mysql:host=localhost;dbname=tomklru270_juliachat';
+	$user_name = 'tomklru270_chatter';
+	$pass_word = 'wachtwoord';
+	$db='tomklru270_juliachat';
+	$uname =   $_POST["username"];
+	$msg =  $_POST["message"];
+	$mykey= $_POST["mykey"];
+ 
+	$connection = new PDO($dsn, $user_name, $pass_word);
+	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	try {
+		$sql = "INSERT INTO chat (username, message, mykey) " .
+		 "VALUES ('$uname', '$msg', '$mykey')";
+		$connection->exec($sql);
+
+		$getid = 'SELECT ID FROM chat WHERE username ="'.$uname.'" AND message ="'.$msg.'"'; // AND message="'."
+		 
+		$statement = $connection->query($getid); 
+		$result = $statement->fetchColumn();	
+		$newid = (int)$result;	
+		echo $newid;
+		
+		}
+
+		catch(PDOException $e) {
+		 echo $sql . "<br>" . $e->getMessage();
+		}
+
+		$connection = null; // Close connection
+
+		if(isset($_SERVER['HTTP_REFERER'])) {
+		    $previous = $_SERVER['HTTP_REFERER'];
+		}
+		// die(header("Location: ".$_SERVER["HTTP_REFERER"]));
+
+} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') { //does stuff if the request method is get
+	if(isset($_GET["action"]) && $_GET['action'] == "list") {
+		 $dsn = 'mysql:host=localhost;dbname=tomklru270_juliachat';
+	$user_name = 'tomklru270_chatter';
+	$pass_word = 'wachtwoord';
+	$db="tomklru270_juliachat";
+		$mykey= $_GET['mykey'];
+		$connection = new PDO($dsn, $user_name, $pass_word);
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		try {
+		$getids = 'SELECT id FROM chat WHERE mykey ="'.$mykey.'"'; // AND message="'."
+		$statement = $connection->query($getids); 
+		$result = $statement->fetchall(\PDO::FETCH_ASSOC);
+		
+		$ids = array();
+
+		foreach($result as $row) {
+			$ids[] = $row["id"];
+		}
+
+		$resultJSON = json_encode($ids);
+
+		echo $resultJSON;
+
+
+		}
+
+
+
+
+
+
+		catch(PDOException $e) {
+		 echo $sql . "<br>" . $e->getMessage();
+		}
+
+		$connection = null; // Close connection
+		if(isset($_SERVER['HTTP_REFERER'])) {
+		    $previous = $_SERVER['HTTP_REFERER'];
+		}
+
+	} elseif(isset($_GET["action"]) && $_GET['action'] == "read") {
+		 $dsn = 'mysql:host=localhost;dbname=tomklru270_juliachat';
+	$user_name = 'tomklru270_chatter';
+	$pass_word = 'wachtwoord';
+	$db="tomklru270_juliachat";
+		$id =  $_GET['id']; // $_POST["id"];
+		$mykey= $_GET['mykey'];
+		$connection = new PDO($dsn, $user_name, $pass_word);
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		try {		
+		$getmsg = 'SELECT message FROM chat WHERE id ="'.$id.'" AND mykey ="'.$mykey.'"'; // AND message="'."
+		$getusr = 'SELECT username FROM chat WHERE id ="'.$id.'" AND mykey ="'.$mykey.'"';
+		$statement1 = $connection->query($getmsg); 
+		$statement2 = $connection->query($getusr); 
+		$result1 = $statement1->fetchcolumn();
+		$result2 = $statement2->fetchcolumn();	
+		$fullmessage = $result2." : ".$result1;	
+		echo $fullmessage;
+		}
+
+		catch(PDOException $e) {
+		echo $sql . "<br>" . $e->getMessage();
+		}
+
+		$connection = null; // Close connection
+
+		if(isset($_SERVER['HTTP_REFERER'])) {
+		    $previous = $_SERVER['HTTP_REFERER'];
+		}
+
+
+	} else {
+		echo "GET REQUEST FAILED AGAIN";
+	
+	};
+
+   
+};
 ?>
+
--- script.js
+++ script.js
@@ -2,42 +2,93 @@
 //GLOBAL VARIABLES
 var xhr = new XMLHttpRequest();
 var uname = ""; // username, overwritten bij start()
+var mykey = "";
+var id = "";
+var correctids = "";
+var highestId =0;
 
 function start(){
-uname = document.getElementById("usernameinput").value;
+	//mykey = document.getElementById("usernameinput").value; mykeyinput
+uname = document.getElementById("usernameinput").value; //mykeyinput
+mykey = document.getElementById("mykeyinput").value; 
 document.getElementById("username").style.display = "none";
 document.getElementById("allchat").style.display = "block";
 window.setInterval(function(){ // calls refreshchat every 3 seconds, so chat is updated.
-	getmessages();
-}, 1000);   
+	getids();
+}, 1000); 
 }
 
-function getmessages(){ //gets messages from api.php (.txt file)
-	xhr.open('GET', 'api.php', true);
- 	xhr.onreadystatechange = function () {
-  if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-    var allmessages=xhr.response; //messages arrive as one giant string, separated by "ENTER"
-	document.getElementById("messages").innerHTML = allmessages.replace(/(?:\r\n|\r|\n)/g, '<br />'); //writes messages to div, replaces enter with <br/>
-	scrollToBottom(); //calls scroll function so newest messages are shown
-  	}
-	};
- 	xhr.send();
-};
+
 
 function sendmessages(){ //sends new messages to api.php, so they can be added to the .txt file.
 	var d = new Date();
 	var time = d.toLocaleTimeString();
 	var msg =  chatform.message.value;  //gets message from form
 	var unameTime="(" +time +") "+uname; //combines username with timestamp
-	var url= "username="+ unameTime +"&message=" + msg; //generates POST url with username and message.
+
+	var url= "username="+ unameTime +"&message=" + msg+ "&mykey=" + mykey; //generates POST url with username and message. 
 	xhr.open("POST","api.php", true); //POST request
 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 
   	xhr.send(url);
-	document.getElementById('inputMessage').value = ""; //resets message input field.
+	document.getElementById('inputMessage').value = ""; 
+	xhr.onload = function () {
+    id= xhr.response;
+    
+};
 }; 
 
 function scrollToBottom(){ //makes sure the chat div automatically scrolls to the bottom 
    var div = document.getElementById("messages");
    div.scrollTop = div.scrollHeight - div.clientHeight;
 } 
+
+
+
+
+
+function getids(){	//accesses a string of all ids that belong to the specified mykey.
+	var idurl = "api.php?action=list&mykey="+mykey; //generates url with mykey.
+	xhr.open('GET', idurl, false);
+	xhr.send();
+	//xhr.onreadystatechange = function () {
+ 		//if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    		var responseids= xhr.response;
+    		var correctids = JSON.parse(responseids);
+    		
+    		for (i = 0; i < correctids.length; i++) {
+		correctids[i] = parseInt(correctids[i]); //saves IDs in the array as integers, so they can be used in other functions.
+	}//allids = JSON.parse(responseids);
+    		for (i = 0; i < correctids.length; i++) { //for loop, calls getmessage(id) for every ID that is higher then the highestId variable.
+		if (correctids[i] > highestId) {
+			
+			getmessages(correctids[i]);
+			var newMessage = xhr.response; 
+			console.log(newMessage);//saves response from every request as a variable.
+			document.getElementById("messages").innerHTML += newMessage + "<br>"; //posts the new message into the chat div as a new line.
+			scrollToBottom(); // automatically scrolls to the last message (at the bottom of the div)
+			
+			highestId = correctids[i]; //saves the last id in the array as the highestid, so the loop doesn't repeat until there is a new message (new higher id).
+			}
+		}
+			//}
+			
+  		//}
+};
+	
+function getmessages(id){ //gets messages from api.php (.txt file)
+	var surl = "api.php?action=read&mykey="+ mykey +"&id=" + id; 
+	xhr.open('GET', surl, false);
+ 	//xhr.onreadystatechange = function () {
+ // if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    var allmessages=xhr.response; //messages arrive as one giant string, separated by "ENTER"
+    //console.log(allmessages);
+	//document.getElementById("messages").innerHTML = allmessages.replace(/(?:\r\n|\r|\n)/g, '<br />'); //writes messages to div, replaces enter with <br/>
+	scrollToBottom(); //calls scroll function so newest messages are shown
+  	//}
+	//};
+ 	xhr.send();
+}; 
+	
+
+
 
